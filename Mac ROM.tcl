c02162f37e043f019330a0a190132d5ba2bcb0db
joevt <joevt@shaw.ca>
2025-09-26 04:40:07 -0700

    Improve Erase Happy Mac and Toolbox Init vectors
    Show label and start using offset32code.

--- Mac ROM.tcl
+++ Mac ROM.tcl
@@ -4859,8 +4859,8 @@ if {$dir_start != 0} {
 			uint32 -hex "Checksum (Chunk 3)"
 			uint32 -hex "Checksum (Chunk 4)"
 			set rom_size [uint32 -hex "ROM Size"]
-			uint32 "Erase Happy Mac Vector"
-			uint32 "Toolbox Init Vector"
+			offset32code "Erase Happy Mac Vector" 0
+			offset32code "Toolbox Init Vector" 0
 		}
 
 		endsection
